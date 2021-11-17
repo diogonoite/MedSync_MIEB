9e302321bcf8627069d9325e024b89c8ef647a70
diogonoite <44338110+diogonoite@users.noreply.github.com>
2021-11-17 23:13:24 +0000

    Delete MedSync directory

--- MedSync/erro_upload_exercicio.php
+++
@@ -1,6 +0,0 @@
-<html>
-<link rel="stylesheet" type="text/css" href="./css/reabilitacao.css">
-<div class="feedback_upload_erro">
-<h1>Não foi possível realizar o upload do ficheiro <i class="fa fa-times-circle-o" aria-hidden="true"></i></h1>
-
-    </html>
--- MedSync/projeto.php
+++
@@ -1,7 +0,0 @@
-<html>
-<link rel="stylesheet" type="text/css" href="./css/mainn.css">
-<div class="informacao">
-<h1>O que é a MedSync?</h1>
-<img class="aleatorio" src="./img/sobre4.png">
-</div>
-</html>
